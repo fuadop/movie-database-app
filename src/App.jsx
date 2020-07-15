a734076d59ce1c8507da9c6a0715ac686a5ed670
fuadop <fuadolatunji@gmail.com>
2020-07-15 23:11:51 +0100

    changing router position

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./pages/Home";
 import Latest from "./pages/Latest";
@@ -8,20 +8,18 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/latest" exact>
-          <Latest />
-        </Route>
-        {/* 404 route*/}
-        <Route path="/">
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/latest" exact>
+        <Latest />
+      </Route>
+      {/* 404 route*/}
+      <Route path="/">
+        <Error />
+      </Route>
+    </Switch>
   );
 }
 
